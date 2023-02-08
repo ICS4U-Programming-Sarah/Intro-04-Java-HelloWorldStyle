f116e9e0e8b54671a6fc224e6b94058c5d18024d
Sarah Andrew <sarah.andrew@stu.ocsb.ca>
2023-02-08 23:30:47 +0000

    Made HelloWorldStyle.

---
+++ HelloWorldStyle.java
@@ -0,0 +1,31 @@
+/**
+* This program executes future plans with style checking.
+*
+* @author  Sarah Andrew
+* @version 1.0
+*
+* @since   2023-02-07
+*/
+public final class HelloWorldStyle {
+
+    /**
+     * This is a private constructor used to satisfy the
+     * style checker.
+     *
+     * @exception IllegalStateException Utility class.
+     * @see IllegalStateException
+     */
+    private HelloWorldStyle() {
+        throw new IllegalStateException("Utility class");
+    }
+
+    /**
+     * This is the main method.
+     *
+     * @param args Unused.
+     */
+    public static void main(String[] args) {
+        System.out.println("In the future, I'd like to go into engineering'");
+        System.out.println("I'm excited to see where it'll take me.");
+    }
+}
